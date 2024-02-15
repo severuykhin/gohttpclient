5a8533e4734082fde2f61533b97dea87a522d0ce
Severuykhin Igor <igor@collabox.dev>
2024-02-15 10:50:38 +0300

    feat: added the ability to send request body as application/x-www-form-urlencoded

--- client_test.go
+++ client_test.go
@@ -2,7 +2,6 @@ package gohttpclient
 
 import (
 	"context"
-	"fmt"
 	"testing"
 )
 
@@ -10,16 +9,124 @@ func TestGet(t *testing.T) {
 	// Your test logic here
 	c := NewHttpClient(HttpClientConfig{})
 
-	params := map[string]any{
-		"features": []string{
-			"utm_source", "utm_medium",
+	// params := map[string]any{
+	// 	"features": []string{
+	// 		"utm_source", "utm_medium",
+	// 	},
+	// }
+
+	params := map[string]interface{}{
+		"offset":   "",
+		"group_id": "279425",
+		"filter": map[string]interface{}{
+			"gender":                   "",
+			"age_from":                 "",
+			"age_to":                   "",
+			"lead_status":              "",
+			"date_first_from":          "",
+			"date_first_to":            "",
+			"date_subscription_from":   "",
+			"date_subscription_to":     "",
+			"date_delivery_from":       "",
+			"date_delivery_to":         "",
+			"date_last_delivery_from":  "",
+			"date_last_delivery_to":    "",
+			"date_first_delivery_from": "",
+			"date_first_delivery_to":   "",
+			"date_away_from":           "",
+			"date_away_to":             "",
+			"var_eq_input":             "",
+			"var_gt_input":             "",
+			"var_lt_input":             "",
+			"var_con_input":            "",
 		},
 	}
 
-	data, err := c.Get(context.Background(), "https://google.com", params, nil)
+	_, err := c.Get(context.Background(), "http://localhost", params, nil)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestPost(t *testing.T) {
+
+	c := NewHttpClient(HttpClientConfig{})
+
+	params := map[string]interface{}{
+		"offset":   "",
+		"group_id": "279425",
+		"filter": map[string]interface{}{
+			"gender":                   "",
+			"age_from":                 "",
+			"age_to":                   "",
+			"lead_status":              "",
+			"date_first_from":          "",
+			"date_first_to":            "",
+			"date_subscription_from":   "",
+			"date_subscription_to":     "",
+			"date_delivery_from":       "",
+			"date_delivery_to":         "",
+			"date_last_delivery_from":  "",
+			"date_last_delivery_to":    "",
+			"date_first_delivery_from": "",
+			"date_first_delivery_to":   "",
+			"date_away_from":           "",
+			"date_away_to":             "",
+			"var_eq_input":             "",
+			"var_gt_input":             "",
+			"var_lt_input":             "",
+			"var_con_input":            "",
+		},
+	}
+
+	// send url encoded request
+	_, err := c.Post(context.Background(), "http://localhost", params, map[string]string{
+		HeaderContentType: HeaderContentTypeValueFormUrlEncoded,
+	})
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	fmt.Println(data)
+	// send json request
+	_, err = c.Post(context.Background(), "http://localhost", params, map[string]string{
+		HeaderContentType: HeaderContentTypeValueApplicationJSON,
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	params2 := struct {
+		Age   int
+		Name  string
+		Some  []int
+		Some2 any
+	}{
+		Age:  32,
+		Name: "igor",
+		Some: []int{1, 2, 3},
+		Some2: struct {
+			Key   string
+			Value string
+		}{
+			Key:   "key1",
+			Value: "value1",
+		},
+	}
+
+	// send url encoded request
+	_, err = c.Post(context.Background(), "http://localhost", params2, map[string]string{
+		HeaderContentType: HeaderContentTypeValueFormUrlEncoded,
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// send json request
+	_, err = c.Post(context.Background(), "http://localhost", params2, map[string]string{
+		HeaderContentType: HeaderContentTypeValueApplicationJSON,
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
 }
---
+++ def.go
@@ -0,0 +1,8 @@
+package gohttpclient
+
+const (
+	HeaderContentType                     string = "Content-Type"
+	HeaderContentTypeValueApplicationJSON string = "application/json"
+	HeaderContentTypeValueFormUrlEncoded  string = "application/x-www-form-urlencoded"
+	HeaderContentTypeMultipartFormData    string = "multipart/form-data"
+)
